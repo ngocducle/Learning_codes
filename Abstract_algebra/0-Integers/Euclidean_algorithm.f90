ba186983bda906b068771bc41e3468b8d95fc0a9
ngocducle <lengocduc1009.lnd@gmail.com>
2026-04-18 22:19:22 +0200

    Euclidean algorithm

---
+++ Abstract_algebra/0-Integers/Euclidean_algorithm.f90
@@ -0,0 +1,16 @@
+! Computer program to find the GCD of two integers a and b 
+! and to write the GCD in the form ax + by = (a,b) 
+
+program Euclidean_algorithm 
+
+implicit none 
+
+integer :: a, b, x, y, gcd 
+
+write(*,'(A)') 'Euclidean algorithm: find the greatest common divisor of two integers a and b'
+write(*,'(A)') 'and express the GCD in the form a*x+b*y'
+
+
+
+
+end program Euclidean_algorithm 
